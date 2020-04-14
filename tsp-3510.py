4cba1da60154777ede57cbee85a34b2cf43f56b5
Sam Shapiro <samshapiro@Sams-MacBook-Pro.local>
2020-04-13 21:08:03 -0400

    Added distance function and formatted node values into floats

--- tsp-3510.py
+++ tsp-3510.py
@@ -1,7 +1,7 @@
 # Sam Shapiro and Kenedy Thorne
 # CS 3510 Final Project
 
-import sys
+import sys, math
 
 # Setup all command line arguments, clear output file
 inputFile = open(sys.argv[1], 'r')
@@ -14,9 +14,16 @@ nodes = []
 # Read input file into nodes as 2D array
 for line in inputFile:
     fields = line.split(" ")
-    fields[2] = fields[2][0:len(fields[2])-1]
-    nodes.append(fields)
+    fields[2] = fields[2][0:len(fields[2])-2]
+    mapFields = map(float, fields)
+    nodes.append(mapFields)
 
+def distance(x1, y1, x2, y2):
+    return math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
+
+dist = distance(nodes[0][1], nodes[0][2], nodes[1][1], nodes[1][2])
 
 outputFile.write("Hello ")
+print(dist)
+print("\n")
 print(nodes)
