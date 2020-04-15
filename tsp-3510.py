f15732b3cdb581c480ceb613bdc79b31832c10b5
Sam Shapiro <samshapiro@Sams-MacBook-Pro.local>
2020-04-15 13:23:59 -0400

    Updated to run in main method, deleted nn version, changed README format

--- tsp-3510.py
+++ tsp-3510.py
@@ -3,43 +3,30 @@
 
 import sys, math, random
 
-# Setup all command line arguments, clear output file
-inputFile = open(sys.argv[1], 'r')
-outputFile = open(sys.argv[2], 'a')
-outputFile.truncate(0)
-timeLimit = sys.argv[3]
-
-nodes = []
-
-# Read input file into nodes as 2D array
-for line in inputFile:
-    fields = line.split(" ")
-    fields[2] = fields[2][0:len(fields[2])-2]
-    mapFields = map(float, fields)
-    nodes.append(mapFields)
+#----------------------------------------------------------#
+# BEGIN GENERAL FUNCTIONS
 
 # Finds cartesian distance between 2 points
 def distance(x1, y1, x2, y2):
     return int(math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2)))
 
 # Find tour length of any given path
-def findTourLength(tour):
+def findTourLength(tour, distances):
     thisTourLength = 0
     for x in range(len(tour)):
         thisTourLength = thisTourLength + distances[tour[x-1] - 1][tour[x] - 1]
     return thisTourLength
 
 # Create 2D array of distances between each node
-distances = [[0 for x in range(len(nodes))] for y in range(len(nodes))]
-for p in range(len(nodes)):
-    for q in range(len(nodes)):
-        distances[p][q] = distance(nodes[p][1], nodes[p][2], nodes[q][1], nodes[q][2])
+def findDistances(nodes):
+    distances = [[0 for x in range(len(nodes))] for y in range(len(nodes))]
+    for p in range(len(nodes)):
+        for q in range(len(nodes)):
+            distances[p][q] = distance(nodes[p][1], nodes[p][2], nodes[q][1], nodes[q][2])
+    return distances
 
 
-#----------------------------------------------------------#
-# STILL NEED TO IMPLEMENT TIMEOUT FUNCTIONALITY
-
-def generateInitialPath():
+def generateInitialPath(nodes, distances):
     initialPaths = [[0 for x in range(len(nodes))] for y in range(10)]
     for c in range(10):
         # Create initial tour in order of appearance starting at a random node
@@ -55,7 +42,7 @@ def generateInitialPath():
     minTourLength = sys.maxsize
     minTourIndex = 0
     for w in range(len(initialPaths)):
-        curLen = findTourLength(initialPaths[w])
+        curLen = findTourLength(initialPaths[w], distances)
         if curLen < minTourLength:
             minTourLength = curLen
             minTourIndex = w
@@ -63,7 +50,7 @@ def generateInitialPath():
 
 
 #----------------------------------------------------------#
-# BEGIN 2-OPT FUNCTIONALITY
+# BEGIN 2-OPT FUNCTIONS
 
 # Function to swap path between 2 nodes in the tour
 def swapInPath(tour, a, b):
@@ -73,16 +60,16 @@ def swapInPath(tour, a, b):
     return arr
 
 # Function to run 2-OPT until no more improvements are found
-def run2OPT(tour):
+def run2OPT(tour, distances):
     foundImprovement = True
     bestTour = tour
-    bestTourLength = findTourLength(tour)
+    bestTourLength = findTourLength(tour, distances)
     while foundImprovement:
         foundImprovement = False
         for f in range(len(bestTour) - 1):
             for g in range(f + 1, len(bestTour)):
                 newTour = swapInPath(bestTour, f, g)
-                newTourLength = findTourLength(newTour)
+                newTourLength = findTourLength(newTour, distances)
                 if newTourLength < bestTourLength:
                     bestTourLength = newTourLength
                     bestTour = newTour
@@ -92,12 +79,37 @@ def run2OPT(tour):
                 break
     return bestTour
 
-firstPath = generateInitialPath()
-finalTour = run2OPT(firstPath)
-finalTourLength = findTourLength(finalTour)
 
-# Write to output file
-outputFile.write(str(finalTourLength))
-outputFile.write("\n")
-for h in range(len(finalTour)):
-    outputFile.write(str(finalTour[h]) + " ")
+#----------------------------------------------------------#
+# BEGIN MAIN FUNCTION
+
+def main():
+    # Setup all command line arguments, clear output file
+    inputFile = open(sys.argv[1], 'r')
+    outputFile = open(sys.argv[2], 'a')
+    outputFile.truncate(0)
+    timeLimit = sys.argv[3]
+
+    nodes = []
+
+    # Read input file into nodes as 2D array
+    for line in inputFile:
+        fields = line.split(" ")
+        fields[2] = fields[2][0:len(fields[2])-2]
+        mapFields = map(float, fields)
+        nodes.append(mapFields)
+
+    distances = findDistances(nodes)
+
+    firstPath = generateInitialPath(nodes, distances)
+    finalTour = run2OPT(firstPath, distances)
+    finalTourLength = findTourLength(finalTour, distances)
+
+    # Write to output file
+    outputFile.write(str(finalTourLength))
+    outputFile.write("\n")
+    for h in range(len(finalTour)):
+        outputFile.write(str(finalTour[h]) + " ")
+
+if __name__ == "__main__":
+    main()
